e2aa6ae8822cc4b5f7e9c3136c0ab854314d8997
Fabio Akita <fabioakita@gmail.com>
2018-03-24 18:40:37 -0300

    covering the #accounts going through method_missing

--- test/web3/eth_test.rb
+++ test/web3/eth_test.rb
@@ -84,4 +84,9 @@ describe Web3::Eth::EthModule, vcr: { record: :new_episodes } do
     txid = @web3.eth.sendTransaction([{from: from, to: to, value: 0.5, gas: 21_000}])
     assert_equal '0xd7e6ef89fd9c7a57bc12fc9219d11dd1e25ce0a0440daea894889d991a68109b', txid
   end
+
+  it '#accounts' do
+    accounts = ["0xa04e958880f9b1557694b6aa6274cd111f5183dc", "0x3f61159ce5e39ac27f3541a3f1f7ebb390a77f17", "0x3c51f8fc120ade8295dda3bb40878322590dcd27", "0x77535eeb8206007caef7e86c12f96685d0ecf0c0", "0xdf5a859f9efb916eb2afacfc977d78e0db282bbd", "0xae6fc061e05aa3b468d569ae6f85517005cacb2f", "0x5f0dae2ce068f147518d2463d4bde64bb9c8b09c", "0xd42050c24da173e52dcb87a2a414af5421ef3d68", "0x7f93a3a5060a69dce4d2428d5213ea15c1e0811f", "0x266ef67cec2f567faab06a163af664944ef9a8bf", "0x583cd9b0f50b0e8b3f1ced668f91d66b4f602b2b", "0xc2c9ed250c18ade71b34303c28676e7283b8bc79", "0xc42b00a6be5964b745e6d67933dcd8a92f3e38b0", "0xb88e69486b3be338c0472adf62399221efaf4bc2", "0xe14a93ac02c5fe511a2a1c320428f68c912aa6a3", "0x1f0159ba31a784dc8a999724ec26d8313ca87d3e", "0xf0d02680f7a6ccdff585f5b4af9067e5de9ade38", "0x6e037a6d38f09fbc34d6cc7f469f3e8f25f5afd1", "0x79a4d1c1064e84296f2df10112edcd1c04ac3269", "0x243afb7bca93b080013d8d8fd9c306a3518dee66"]
+    assert_equal accounts, @web3.eth.accounts
+  end
 end
